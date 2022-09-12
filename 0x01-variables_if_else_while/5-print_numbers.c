7a90c17a53476ed9aca11f8190119e089580a5ee
Emperor7656 <youngofficer9@gmail.com>
2022-09-11 18:49:06 -0700

    my print alphabetcode

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digits of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		printf("%d", m);
+	}
+	putchar('\n')
+		return (0)
+}
