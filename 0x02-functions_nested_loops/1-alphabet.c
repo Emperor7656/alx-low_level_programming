2504bf63bcc80ff6950cbed9c652b05f15c85064
Emperor7656 <youngofficer9@gmail.com>
2022-09-13 07:37:23 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - this is the function to print alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_purchar('\n');
+	return (0);
+}
